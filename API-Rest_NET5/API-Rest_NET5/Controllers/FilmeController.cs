f7137867968c1c4001deedcdcf8df3a405c2c6e0
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2022-03-25 17:37:56 -0300

    ajuste

---
+++ API-Rest_NET5/API-Rest_NET5/Controllers/FilmeController.cs
@@ -0,0 +1,29 @@
+﻿using API_Rest_NET5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API_Rest_NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //referencia o controlle da classe, não é necessaria colocar o nome
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdiconarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            System.Console.WriteLine(filme.Titulo);
+
+            if(filme.Titulo == "")
+            {
+                for(int i = 0; i < 1000; i++)
+                {
+                    System.Console.WriteLine("Olá Mundo!!");
+
+                }
+            }
+        }
+    }
+}
